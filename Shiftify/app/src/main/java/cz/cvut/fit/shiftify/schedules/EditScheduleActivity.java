124d9ed0dc7cf06efab258df528babaa40ed65da
Petr Pansky <pansky2006@seznam.cz>
2016-11-13 23:33:28 +0100

    Refactoring - schedule activities

---
+++ Shiftify/app/src/main/java/cz/cvut/fit/shiftify/schedules/EditScheduleActivity.java
@@ -0,0 +1,10 @@
+package cz.cvut.fit.shiftify.schedules;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Petr on 11/13/16.
+ */
+
+public class EditScheduleActivity extends AppCompatActivity {
+}
