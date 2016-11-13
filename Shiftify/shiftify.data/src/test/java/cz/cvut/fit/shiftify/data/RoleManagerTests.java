2031bd1ccf62c55682a6f6fe7127fa83a660e587
lukas.v.komarek@email.cz <komarlu2@fit.cvut.cz>
2016-11-13 15:33:13 +0100

    Adds some basic tests to show how to proceed with it in case other programmers wouldn't be satisfied with the current dummy implementation and would need something more clever.

---
+++ Shiftify/shiftify.data/src/test/java/cz/cvut/fit/shiftify/data/RoleManagerTests.java
@@ -0,0 +1,29 @@
+package cz.cvut.fit.shiftify.data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class RoleManagerTests {
+    @Test
+    public void dummyMethods_dontThrowException() throws Exception {
+        //assertEquals(4, 2 + 2);
+        RoleManager rm = new RoleManager();
+        Role r = new Role("admin");
+        try { rm.add(r); }
+        catch(Exception ex) { assert("RoleManager add method throws exception." == null); }
+        try { rm.edit(r.getId(), new Role("administrator")); }
+        catch(Exception ex) { assert("RoleManager edit method throws exception." == null); }
+        try { rm.role(r.getId()); }
+        catch(Exception ex) { assert("RoleManager role method throws exception." == null); }
+        try { rm.roles(); }
+        catch(Exception ex) { assert("RoleManager roles method throws exception." == null); }
+        try { rm.delete(r.getId()); }
+        catch(Exception ex) { assert("RoleManager delete method throws exception." == null); }
+    }
+}
---
+++ Shiftify/shiftify.data/src/test/java/cz/cvut/fit/shiftify/data/ScheduleTypeManagerTests.java
@@ -0,0 +1,32 @@
+package cz.cvut.fit.shiftify.data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ScheduleTypeManagerTests {
+    @Test
+    public void dummyMethods_dontThrowException() throws Exception {
+        //assertEquals(4, 2 + 2);
+        ScheduleTypeManager stm = new ScheduleTypeManager();
+        ScheduleType st = new ScheduleType();
+        ScheduleShift ss = new ScheduleShift();
+        try { stm.add(st); }
+        catch(Exception ex) { assert("RoleManager add method throws exception." == null); }
+        try { stm.edit(st.getId(), st); }
+        catch(Exception ex) { assert("RoleManager edit method throws exception." == null); }
+        try { stm.addShift(st.getId(), ss); }
+        catch(Exception ex) { assert("RoleManager addShift method throws exception." == null); }
+        try { stm.editShift(ss.getId(), ss); }
+        catch(Exception ex) { assert("RoleManager editShift method throws exception." == null); }
+        try { stm.deleteShift(ss.getId()); }
+        catch(Exception ex) { assert("RoleManager deleteShift method throws exception." == null); }
+        try { stm.delete(st.getId()); }
+        catch(Exception ex) { assert("RoleManager delete method throws exception." == null); }
+    }
+}
