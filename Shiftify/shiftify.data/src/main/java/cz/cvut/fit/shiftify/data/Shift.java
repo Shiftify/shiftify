f485c04f919c21f5a526636f0f850d9c9c7deca2
lukas.v.komarek@email.cz <komarlu2@fit.cvut.cz>
2016-11-22 13:53:45 +0100

    Fixes a bug in #Shift.persistsIntoNextDay() and adds the found bug in its test method.

--- Shiftify/shiftify.data/src/main/java/cz/cvut/fit/shiftify/data/Shift.java
+++ Shiftify/shiftify.data/src/main/java/cz/cvut/fit/shiftify/data/Shift.java
@@ -1,6 +1,12 @@
 package cz.cvut.fit.shiftify.data;
 
-import java.sql.Time;
+        import java.sql.Time;
+        import java.sql.Date;
+        import java.text.SimpleDateFormat;
+        import java.util.Calendar;
+        import java.util.GregorianCalendar;
+        import java.util.Locale;
+        import java.util.TimeZone;
 
 /**
  * Created by lukas on 11.11.2016.
@@ -44,7 +50,10 @@ abstract public class Shift {
     public boolean persistsIntoNextDay() throws Exception {
         if (from == null || duration == null)
             throw new Exception("Null attribute (From or Duration or both) in Shift object.");
-        Time t = new Time(from.getHours() + duration.getHours(),
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        int fromDays = Date.valueOf(sdf.format(from)).getDate() - 1;
+        int durationDays = Date.valueOf(sdf.format(duration)).getDate() - 1;
+        Time t = new Time((fromDays + durationDays)*24 + from.getHours() + duration.getHours(),
                 from.getMinutes() + duration.getMinutes(), 0);
         return t.after(new Time(24, 0, 0));
     }
