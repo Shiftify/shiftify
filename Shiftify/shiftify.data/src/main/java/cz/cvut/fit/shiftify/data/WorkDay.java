a3f298694d5af642ee12650763d780b7febb534d
Lukáš Komárek <lukas.v.komarek@email.cz>
2017-02-07 16:42:54 +0100

    Adds comparison method to WorkDay.

--- Shiftify/shiftify.data/src/main/java/cz/cvut/fit/shiftify/data/WorkDay.java
+++ Shiftify/shiftify.data/src/main/java/cz/cvut/fit/shiftify/data/WorkDay.java
@@ -1,5 +1,6 @@
 package cz.cvut.fit.shiftify.data;
 
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
@@ -9,8 +10,9 @@ import cz.cvut.fit.shiftify.data.models.Shift;
  * Created by lukas on 11.11.2016.
  */
 
-public class WorkDay {
-    public WorkDay() { }
+public class WorkDay implements Comparator<WorkDay>, Comparable<WorkDay> {
+    // Constructors
+    public WorkDay() { this(null, null); }
     public WorkDay(Date date) { // no work this day
         this(date, null);
     }
@@ -22,7 +24,7 @@ public class WorkDay {
     protected Date date;
     protected List<Shift> shifts;
 
-    // getters and setters
+    // Getters and setters
     public Date getDate() {
         return date;
     }
@@ -38,5 +40,23 @@ public class WorkDay {
 
     public boolean hasShifts() {
          return shifts == null || shifts.isEmpty();
+    }
+
+    // Comparator methods
+    @Override
+    public int compareTo(WorkDay workDay) {
+        return compare(this, workDay);
+    }
+    @Override
+    public int compare(WorkDay workDay1, WorkDay workDay2) {
+        if (workDay1.shifts.size() == 0) {
+            if (workDay2.shifts.size() == 0)
+                return 0;
+            return 1;
+        }
+        if (workDay2.shifts.size() == 0)
+            return -1;
+        long diff = workDay1.shifts.get(0).getFrom().getTimeInMillis() - workDay2.shifts.get(0).getFrom().getTimeInMillis();
+        return (diff == 0 ? 0 : (diff > 0 ? 1 : -1));
     }
 }
