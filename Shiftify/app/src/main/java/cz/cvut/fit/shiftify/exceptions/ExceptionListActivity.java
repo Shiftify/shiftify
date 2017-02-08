3400497a28d4e0e5f432bf59baad591ae5b9899d
Petr Panský <pansky2006@seznam.cz>
2017-02-08 15:39:49 +0100

    Adds update method into exception adapter

--- Shiftify/app/src/main/java/cz/cvut/fit/shiftify/exceptions/ExceptionListActivity.java
+++ Shiftify/app/src/main/java/cz/cvut/fit/shiftify/exceptions/ExceptionListActivity.java
@@ -1,16 +1,19 @@
 package cz.cvut.fit.shiftify.exceptions;
 
+import android.app.DialogFragment;
 import android.app.ListFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -21,6 +24,7 @@ import cz.cvut.fit.shiftify.R;
 import cz.cvut.fit.shiftify.data.managers.UserManager;
 import cz.cvut.fit.shiftify.data.models.ExceptionShift;
 import cz.cvut.fit.shiftify.data.models.User;
+import cz.cvut.fit.shiftify.schedules.ScheduleEditActivity;
 import cz.cvut.fit.shiftify.utils.ToolbarUtils;
 
 /**
@@ -32,6 +36,7 @@ public class ExceptionListActivity extends AppCompatActivity {
     public static final String USER_ID = "user_id";
     public static final String EXCEPTION_ID = "exception_id";
     public static final String TAG = "EXCEPTION_LIST_ACTIVITY";
+    public static final String DELETE_DIALOG = "exception_delete_dialog";
     private User mUser;
 
 
@@ -65,7 +70,9 @@ public class ExceptionListActivity extends AppCompatActivity {
 
     public static class ExceptionListFragment extends ListFragment {
 
-        private ArrayAdapter<ExceptionShift> mExceptionAdapter;
+        private static final int CREATE_EXCEPTION_REQUEST = 0;
+        private static final int EDIT_EXCEPTION_REQUEST = 1;
+        private ExceptionAdapter mExceptionAdapter;
         private User mUser;
         private UserManager mUserManager;
         private FloatingActionButton mAddFloatingButton;
@@ -85,7 +92,7 @@ public class ExceptionListActivity extends AppCompatActivity {
                 public void onClick(View view) {
                     Intent intent = new Intent(getActivity(), ExceptionEditActivity.class);
                     intent.putExtra(USER_ID, mUser.getId());
-                    startActivity(intent);
+                    startActivityForResult(intent, CREATE_EXCEPTION_REQUEST);
                 }
             });
             return view;
@@ -95,10 +102,9 @@ public class ExceptionListActivity extends AppCompatActivity {
         public void onActivityCreated(Bundle savedInstanceState) {
             super.onActivityCreated(savedInstanceState);
             mUser = ((ExceptionListActivity) getActivity()).mUser;
-            List<ExceptionShift> exceptionNewList = new ArrayList<>(mUserManager.getAllExceptionShifts(mUser.getId()));
-            ExceptionShift[] exceptions = exceptionNewList.toArray(new ExceptionShift[exceptionNewList.size()]);
-            mExceptionAdapter = new ExceptionAdapter(getActivity(), R.layout.list_item_exception, exceptions);
+            mExceptionAdapter = new ExceptionAdapter(getActivity(), R.layout.list_item_exception);
             setListAdapter(mExceptionAdapter);
+            updateExceptionList();
         }
 
         @Override
@@ -108,6 +114,52 @@ public class ExceptionListActivity extends AppCompatActivity {
             intent.putExtra(EXCEPTION_ID, exception.getId());
             intent.putExtra(USER_ID, mUser.getId());
             startActivity(intent);
+        }
+
+        @Override
+        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+            if (v.getId() == android.R.id.list) {
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+                menu.setHeaderTitle(R.string.choose_action);
+                menu.add(Menu.NONE, R.string.edit, Menu.NONE, getString(R.string.edit));
+                menu.add(Menu.NONE, R.string.delete, Menu.NONE, getString(R.string.delete));
+            }
+        }
+
+        @Override
+        public boolean onContextItemSelected(MenuItem item) {
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+            switch (item.getItemId()) {
+                case R.string.edit: // Edit
+                    startEditActivity(mExceptionAdapter.getItem(info.position));
+                    return true;
+                case R.string.delete: // Delete
+                    showDeleteDialog(mExceptionAdapter.getItem(info.position));
+                    return true;
+                default:
+                    return super.onContextItemSelected(item);
+            }
+        }
+
+        private void startEditActivity(ExceptionShift exceptionShift) {
+            Intent intent = new Intent(getActivity(), ScheduleEditActivity.class);
+            intent.putExtra(EXCEPTION_ID, exceptionShift.getId());
+            intent.putExtra(USER_ID, mUser.getId());
+            startActivityForResult(intent, EDIT_EXCEPTION_REQUEST);
+        }
+
+        void showDeleteDialog(ExceptionShift exceptionShift) {
+            DialogFragment newFragment = ExceptionDeleteDialog.newInstance();
+            Bundle userBundle = new Bundle();
+            userBundle.putLong(EXCEPTION_ID, exceptionShift.getId());
+            newFragment.setArguments(userBundle);
+            newFragment.show(getFragmentManager(), DELETE_DIALOG);
+        }
+
+
+        public void updateExceptionList() {
+            List<ExceptionShift> exceptionNewList = new ArrayList<>(mUserManager.getAllExceptionShifts(mUser.getId()));
+            mExceptionAdapter.setExceptionShifts(exceptionNewList);
         }
     }
 }
