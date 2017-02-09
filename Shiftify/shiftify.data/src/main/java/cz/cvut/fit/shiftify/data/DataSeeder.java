951b835f291ee6391a74b49db6b9978383fb8ed5
Lukáš Komárek <lukas.v.komarek@email.cz>
2017-02-09 13:28:57 +0100

    Updates seeder.

--- Shiftify/shiftify.data/src/main/java/cz/cvut/fit/shiftify/data/DataSeeder.java
+++ Shiftify/shiftify.data/src/main/java/cz/cvut/fit/shiftify/data/DataSeeder.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Random;
 
 import cz.cvut.fit.shiftify.data.managers.ScheduleTypeManager;
 import cz.cvut.fit.shiftify.data.managers.UserManager;
@@ -67,12 +68,12 @@ public class DataSeeder {
             e.printStackTrace();
         }
 
-        ScheduleType scheduleType = new ScheduleType("Železárny", 7, "Slouží jako rozpis pro vrátnýho.");
         List<ScheduleShift> shifts = new ArrayList<>();
         shifts.add(new ScheduleShift("1. ranní", Utilities.GregCalFrom(6, 0), Utilities.GregCalFrom(8, 0), 2, "To se bude blbě vstávat."));
         shifts.add(new ScheduleShift("noční", Utilities.GregCalFrom(22, 0), Utilities.GregCalFrom(8, 0), 3));
         shifts.add(new ScheduleShift("2. ranní", Utilities.GregCalFrom(6, 0), Utilities.GregCalFrom(8, 0), 5, "To se bude blbě vstávat."));
         shifts.add(new ScheduleShift("odpolední", Utilities.GregCalFrom(14, 0), Utilities.GregCalFrom(8, 0),  6));
+        ScheduleType scheduleType = new ScheduleType("Železárny", 6, "Slouží jako rozpis pro vrátnýho.");
         scheduleType.setShifts(shifts);
 
         try {
@@ -114,9 +115,9 @@ public class DataSeeder {
         for (User user : userManager.allUsers()) {
             List<Schedule> schedules = new ArrayList<>();
             schedules.add(new Schedule(user.getId(), scheduleTypes.get(0).getId(),
-                    new GregorianCalendar(2016, 10, 2), new GregorianCalendar(2017, 10, 1), 6));
+                    new GregorianCalendar(2016, 10, 2), new GregorianCalendar(2017, 10, 1), new Random().nextInt(6)+1));
             schedules.add(new Schedule(user.getId(), scheduleTypes.get(1).getId(),
-                    new GregorianCalendar(2017, 10, 2), new GregorianCalendar(2017, 10, 10), 1));
+                    new GregorianCalendar(2017, 10, 2), new GregorianCalendar(2017, 10, 10), new Random().nextInt(8)+1));
             for (Schedule schedule : schedules) {
                 try {
                     userManager.addSchedule(schedule);
