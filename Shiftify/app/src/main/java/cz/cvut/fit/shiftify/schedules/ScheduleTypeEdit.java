efd68706a19f3759b5487cc5737eafe322fbe75c
liferili <liferov.ilia@gmail.com>
2017-04-23 21:35:38 +0200

    Layouts for schedule type editor added

---
+++ Shiftify/app/src/main/java/cz/cvut/fit/shiftify/schedules/ScheduleTypeEdit.java
@@ -0,0 +1,15 @@
+package cz.cvut.fit.shiftify.schedules;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cz.cvut.fit.shiftify.R;
+
+public class ScheduleTypeEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_type_edit);
+    }
+}
