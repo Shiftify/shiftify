bab510e0d2d3fcb349d16fc0c9edd10228312823
liferili <liferov.ilia@gmail.com>
2017-05-24 13:04:49 +0200

    Duplicate method fix

--- Shiftify/app/src/main/java/cz/cvut/fit/shiftify/main/MainActivity.java
+++ Shiftify/app/src/main/java/cz/cvut/fit/shiftify/main/MainActivity.java
@@ -247,11 +247,6 @@ public class MainActivity extends AppCompatActivity implements DatePickDialog.Da
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
     private void onImportIntent(Intent intent){
         ExportImportFragment fragment = (ExportImportFragment) getSupportFragmentManager().findFragmentByTag(EXPORT_IMPORT_FRAGMENT_TAG);
         fragment.onImportRequested(intent.getData());
